98c85583c74a058b21c1333e93a1dc54e5a01047
Tomas Suarez <83978153+datatomas@users.noreply.github.com>
2026-06-13 19:22:21 -0400

    feat: se actualizan tags para lab de affinity y taints para simular funcionamiento de system pool

--- kubernetes/labels.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl label node minikube gpu=nvidia --overwrite
-
-kubectl label node minikube-m02 gpu=other --overwrite
-kubectl label node minikube-m02 cpu=ryzen --overwrite
---
+++ kubernetes/node_labels_taints.sh
@@ -0,0 +1,28 @@
+kubectl label node minikube \
+  pool=system \
+  workload=system \
+  disk=ssd \
+  zone=zone-a \
+  --overwrite
+
+kubectl label node minikube-m02 \
+  pool=user \
+  workload=ai \
+  gpu=nvidia \
+  cpu=ryzen \
+  disk=ssd \
+  zone=zone-b \
+  --overwrite
+
+kubectl label node minikube-m03 \
+  pool=user \
+  workload=frontend \
+  cpu=intel \
+  disk=ssd \
+  zone=zone-c \
+  --overwrite
+#Para bloquear apps normales ne system
+kubectl taint node minikube CriticalAddonsOnly=true:NoSchedule --overwrite
+# verify
+kubectl get nodes -L pool,workload,gpu,cpu,disk,zone
+kubectl describe node minikube | grep -i taint
